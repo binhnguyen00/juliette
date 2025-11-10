d1888e3702f69684a440f52a4e93f6da2566be46
Binh Nguyen <jackjack2000.kahp@gmail.com>
2025-11-10 11:09:49 +0700

    combind react-stately with tanstack query

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,72 +1,81 @@
 import React from "react";
 import axios from "axios";
-
+import { useAsyncList } from "react-stately";
 import { useQuery } from "@tanstack/react-query";
 import { Spinner, Button, Table, TableHeader, TableRow, TableBody, TableCell, TableColumn } from "@heroui/react";
 
 import { DefaultLayout } from "@components";
 
-export function Home() {
+export default function Home() {
   return (
     <DefaultLayout>
       <div className="space-y-4">
         <p className="text-2xl font-bold text-center">
           React + Vite + TailwindCSS + HeroUI
         </p>
-        <MockUsers />
       </div>
+      <MockUsers />
     </DefaultLayout>
   );
 }
 
 function MockUsers() {
-  const { data, isLoading, isError, error, refetch } = useQuery({
+  const { data, isLoading, refetch } = useQuery({
     queryKey: ["users"],
     queryFn: () => axios.get("https://jsonplaceholder.typicode.com/users").then((res) => res.data),
   });
 
-  const [ users, setUsers ] = React.useState<any[]>([]);
-
-  const refresh = () => {
-    refetch().then(() => {
-      if (data) {
-        setUsers([...data].sort(() => Math.random() - 0.5));
+  const list = useAsyncList({
+    async load() {
+      return {
+        items: data || []
       }
-    });
-  };
+    },
+    async sort({ items, sortDescriptor }) {
+      return {
+        items: items.sort((a: any, b: any) => {
+          let first = a[sortDescriptor.column];
+          let second = b[sortDescriptor.column];
+          let cmp = (parseInt(first) || first) < (parseInt(second) || second) ? -1 : 1;
+
+          if (sortDescriptor.direction === "descending") {
+            cmp *= -1;
+          }
+
+          return cmp;
+        }),
+      };
+    },
+  });
 
   React.useEffect(() => {
-    if (data) setUsers(data);
-  }, [data]);
-
-  if (isError) return (
-    <div className="w-full h-fullflex flex-col justify-center items-center">
-      <p className="text-red-500">{error.message}</p>
-    </div>
-  );
+    list.reload();
+  }, [data])
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-start space-y-4">
-      <Table 
+      <Table
         aria-label="Mock Users"
         classNames={{
           table: "min-h-[400px] min-w-[600px]",
         }}
+        sortDescriptor={list.sortDescriptor}
+        onSortChange={list.sort}
       >
         <TableHeader>
           <TableColumn> No. </TableColumn>
           <TableColumn> Id </TableColumn>
-          <TableColumn> Name </TableColumn>
-          <TableColumn> Username </TableColumn>
-          <TableColumn> Email </TableColumn>
-          <TableColumn> Phone </TableColumn>
-          <TableColumn> Website </TableColumn>
+          <TableColumn key="name" allowsSorting> Name </TableColumn>
+          <TableColumn key="username" allowsSorting> Username </TableColumn>
+          <TableColumn key="email" allowsSorting> Email </TableColumn>
+          <TableColumn key="phone" allowsSorting> Phone </TableColumn>
+          <TableColumn key="website" allowsSorting> Website </TableColumn>
         </TableHeader>
         <TableBody
           isLoading={isLoading}
           loadingContent={<Spinner label="Loading..." />}
         >
-          {users.map((user: any, i: number) => (
+          {list.items.map((user: any, i: number) => (
             <TableRow key={user.id}>
               <TableCell>{i + 1}</TableCell>
               <TableCell>{user.id}</TableCell>
@@ -79,9 +88,9 @@ function MockUsers() {
           ))}
         </TableBody>
       </Table>
-      <Button onPress={refresh} color="primary" size="md" className="self-end">
+      <Button onPress={() => list.reload()} color="primary" size="md" className="self-end">
         Refresh
       </Button>
     </div>
-  );
+  )
 }
